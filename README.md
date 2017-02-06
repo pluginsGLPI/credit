2cc6a81970d20f7673bed1c975b91d72031bffe6
François Legastelois <flegastelois@teclib.com>
2017-02-06 16:49:20 +0100

    first commit, create from empty plugin

---
+++ README.md
@@ -0,0 +1,11 @@
+# Intervention GLPI plugin
+
+With this plugin you can manage intervention voucher for your helpdesk service.
+
+## Contributing
+
+* Open a ticket for each bug/feature so it can be discussed
+* Follow [development guidelines](http://glpi-developer-documentation.readthedocs.io/en/latest/plugins.html)
+* Refer to [GitFlow](http://git-flow.readthedocs.io/) process for branching
+* Work on a new branch on your own fork
+* Open a PR that will be reviewed by a developer
---
+++ setup.php
@@ -0,0 +1,96 @@
+<?php
+/*
+ -------------------------------------------------------------------------
+ intervention plugin for GLPI
+ Copyright (C) 2017 by the intervention Development Team.
+
+ https://github.com/pluginsGLPI/intervention
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of intervention.
+
+ intervention is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ intervention is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with intervention. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+define('PLUGIN_INTERVENTION_VERSION', '1.0.0');
+
+/**
+ * Init hooks of the plugin.
+ * REQUIRED
+ *
+ * @return void
+ */
+function plugin_init_intervention() {
+   global $PLUGIN_HOOKS;
+
+   $PLUGIN_HOOKS['csrf_compliant']['intervention'] = true;
+}
+
+
+/**
+ * Get the name and the version of the plugin
+ * REQUIRED
+ *
+ * @return array
+ */
+function plugin_version_intervention() {
+   return [
+      'name'           => 'intervention',
+      'version'        => PLUGIN_INTERVENTION_VERSION,
+      'author'         => '<a href="http://www.teclib.com">Teclib\'</a>',
+      'license'        => 'GPLv3',
+      'homepage'       => 'https://github.com/TECLIB/intervention',
+      'minGlpiVersion' => '9.1'
+   ];
+}
+
+/**
+ * Check pre-requisites before install
+ * OPTIONNAL, but recommanded
+ *
+ * @return boolean
+ */
+function plugin_intervention_check_prerequisites() {
+   // Strict version check (could be less strict, or could allow various version)
+   if (version_compare(GLPI_VERSION, '9.1', 'lt')) {
+      if (method_exists('Plugin', 'messageIncompatible')) {
+         echo Plugin::messageIncompatible('core', '9.1');
+      } else {
+         echo "This plugin requires GLPI >= 9.1";
+      }
+      return false;
+   }
+   return true;
+}
+
+/**
+ * Check configuration process
+ *
+ * @param boolean $verbose Whether to display message on failure. Defaults to false
+ *
+ * @return boolean
+ */
+function plugin_intervention_check_config($verbose = false) {
+   if (true) { // Your configuration check
+      return true;
+   }
+
+   if ($verbose) {
+      _e('Installed / not configured', 'intervention');
+   }
+   return false;
+}
