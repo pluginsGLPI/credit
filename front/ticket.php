d60d110675a1d26dfebb3a1c84d6ba0b4bddb88a
Naylin Medina <nmedina@teclib.com>
2018-04-30 15:52:49 -0400

    docs(header): change license header
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- front/ticket.php
+++ front/ticket.php
@@ -1,29 +1,26 @@
 <?php
-/*
- -------------------------------------------------------------------------
- credit plugin for GLPI
- Copyright (C) 2017 by the credit Development Team.
-
- https://github.com/pluginsGLPI/credit
- -------------------------------------------------------------------------
-
- LICENSE
-
- This file is part of credit.
-
- credit is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- credit is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with credit. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
+/**
+ * --------------------------------------------------------------------------
+ * LICENSE
+ *
+ * This file is part of credit.
+ *
+ * credit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * credit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * --------------------------------------------------------------------------
+ * @author    François Legastelois
+ * @copyright Copyright (C) 2017-2018 by Teclib'.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/pluginsGLPI/credit
+ * @link      https://pluginsglpi.github.io/credit/
+ * -------------------------------------------------------------------------
  */
 
  /** @file
