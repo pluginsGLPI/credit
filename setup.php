180be47e27b1f33ce4f6c8e4f23f6e4a242a8e8b
Cédric Anne <cedric.anne@gmail.com>
2018-06-19 15:08:48 +0200

    Add 1.2.0 version definition

--- setup.php
+++ setup.php
@@ -23,7 +23,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_CREDIT_VERSION', '1.1.1');
+define('PLUGIN_CREDIT_VERSION', '1.2.0');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_CREDIT_MIN_GLPI", "9.2");
