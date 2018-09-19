4b8b9f83b8441246c9c569f2a3232cf7870e393e
Xacobo Freire <xacobo@ticgal.com>
2018-09-19 12:13:42 +0200

    Add notification for credit expired

--- hook.php
+++ hook.php
@@ -44,6 +44,17 @@ function plugin_credit_install() {
          }
       }
    }
+
+   CronTask::register(
+      'PluginCreditEntity',
+      'creditexpired',
+      DAY_TIMESTAMP,
+      [
+         'comment' => '',
+         'mode' => CronTask::MODE_EXTERNAL,
+      ]
+   );
+
    return true;
 }
 
