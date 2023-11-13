8eea56e60b208f7b73dde8fc0e2914f681802f02
Rom1-B <rbiot@teclib.com>
2023-11-13 14:02:26 +0100

    fix(ticket): error when consuming from a ticket

--- hook.php
+++ hook.php
@@ -125,13 +125,13 @@ function plugin_credit_get_datas(NotificationTargetTicket $target)
         SELECT
             `glpi_plugin_credit_entities`.`name`,
             `glpi_plugin_credit_entities`.`quantity`,
-            (SELECT SUM(`glpi_plugin_credit_tickets`.`consumed`) FROM `glpi_plugin_credit_tickets` WHERE `glpi_plugin_credit_tickets`.`plugin_credit_entities_id` = `glpi_plugin_credit_entities`.`id` AND `glpi_plugin_credit_tickets`.`tickets_id` = {$id}) AS `consumed_on_ticket`,
+            (SELECT SUM(`glpi_plugin_credit_tickets`.`consumed`) FROM `glpi_plugin_credit_tickets` WHERE `glpi_plugin_credit_tickets`.`plugin_credit_entities_id` = `glpi_plugin_credit_entities`.`id` AND `glpi_plugin_credit_tickets`.`tickets_id` = {(int)$id}) AS `consumed_on_ticket`,
             (SELECT SUM(`glpi_plugin_credit_tickets`.`consumed`) FROM `glpi_plugin_credit_tickets` WHERE `glpi_plugin_credit_tickets`.`plugin_credit_entities_id` = `glpi_plugin_credit_entities`.`id`) AS  `consumed_total`
         FROM `glpi_plugin_credit_entities`
-        WHERE `is_active`=1 and `entities_id`={$entity_id}
+        WHERE `is_active`=1 and `entities_id`={(int)$entity_id}
 SQL;
 
-    foreach ($DB->request($query) as $credit) {
+    foreach ($DB->query($query) as $credit) {
         $target->data["credit.ticket"][] = [
             '##credit.voucher##' => $credit['name'],
             '##credit.used##'    => (int)$credit['consumed_on_ticket'],
