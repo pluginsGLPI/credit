b37fb1b406ddfac1dc1f49596ecc530c542a594f
Romain B <8530352+Rom1-B@users.noreply.github.com>
2023-11-14 15:42:25 +0100

    fix(ticket): error when consuming from a ticket

--- hook.php
+++ hook.php
@@ -121,15 +121,42 @@ function plugin_credit_get_datas(NotificationTargetTicket $target)
     $ticket->getFromDB($id);
     $entity_id = $ticket->fields['entities_id'];
 
-    $query = <<<SQL
-        SELECT
-            `glpi_plugin_credit_entities`.`name`,
-            `glpi_plugin_credit_entities`.`quantity`,
-            (SELECT SUM(`glpi_plugin_credit_tickets`.`consumed`) FROM `glpi_plugin_credit_tickets` WHERE `glpi_plugin_credit_tickets`.`plugin_credit_entities_id` = `glpi_plugin_credit_entities`.`id` AND `glpi_plugin_credit_tickets`.`tickets_id` = {$id}) AS `consumed_on_ticket`,
-            (SELECT SUM(`glpi_plugin_credit_tickets`.`consumed`) FROM `glpi_plugin_credit_tickets` WHERE `glpi_plugin_credit_tickets`.`plugin_credit_entities_id` = `glpi_plugin_credit_entities`.`id`) AS  `consumed_total`
-        FROM `glpi_plugin_credit_entities`
-        WHERE `is_active`=1 and `entities_id`={$entity_id}
-SQL;
+    $query = [
+        'SELECT' => [
+            'name',
+            'quantity',
+            'consumed_on_ticket' => new QuerySubQuery([
+                'SELECT' => [
+                    'SUM' => 'consumed AS consumed_on_ticket',
+                ],
+                'FROM' => [
+                    'glpi_plugin_credit_tickets',
+                ],
+                'WHERE' => [
+                    'plugin_credit_entities_id' => new QueryExpression($DB->quoteName('glpi_plugin_credit_entities.id')),
+                    'tickets_id' => $id,
+                ],
+            ]),
+            'consumed_total' => new QuerySubQuery([
+                'SELECT' => [
+                    'SUM' => 'consumed AS consumed_total',
+                ],
+                'FROM' => [
+                    'glpi_plugin_credit_tickets',
+                ],
+                'WHERE' => [
+                    'plugin_credit_entities_id' => new QueryExpression($DB->quoteName('glpi_plugin_credit_entities.id')),
+                ],
+            ]),
+        ],
+        'FROM' => [
+            'glpi_plugin_credit_entities',
+        ],
+        'WHERE' => [
+            'is_active' => 1,
+            'entities_id' => $entity_id,
+        ],
+    ];
 
     foreach ($DB->request($query) as $credit) {
         $target->data["credit.ticket"][] = [
