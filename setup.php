9dc141caf6af00a2a95e80f2e1dda5ff1e5c29a2
Romain B <8530352+Rom1-B@users.noreply.github.com>
2024-02-21 09:04:19 +0100

    1.13.2 (#128)

--- setup.php
+++ setup.php
@@ -29,7 +29,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_CREDIT_VERSION', '1.13.1');
+define('PLUGIN_CREDIT_VERSION', '1.13.2');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_CREDIT_MIN_GLPI", "10.0.0");
