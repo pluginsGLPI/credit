b2eefc6b4acef808519be626df76945c395b62db
Rom1-B <rom1.biot@gmail.com>
2025-05-21 10:17:30 +0200

    version

--- setup.php
+++ setup.php
@@ -29,7 +29,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_CREDIT_VERSION', '1.14.0');
+define('PLUGIN_CREDIT_VERSION', '1.15.0-beta1');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_CREDIT_MIN_GLPI", "11.0.0");
