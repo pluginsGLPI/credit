ce979d2fbcec0900d49ceaffac89d65fb3386605
Stanislas <skita@teclib.com>
2025-07-08 15:22:22 +0200

    bump setup

--- setup.php
+++ setup.php
@@ -29,7 +29,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_CREDIT_VERSION', '1.14.1');
+define('PLUGIN_CREDIT_VERSION', '1.15.0-beta3');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_CREDIT_MIN_GLPI", "11.0.0");
